5053fb27bd7751dd129e6ab842166e75e4eab3cc
Adrián <adriiiprieto@gmail.com>
2021-02-08 20:13:00 +0100

    F001 Coroutines

--- app/src/main/java/es/adriiiprieto/nasaapi/ui/fragment/list/ListViewModel.kt
+++ app/src/main/java/es/adriiiprieto/nasaapi/ui/fragment/list/ListViewModel.kt
@@ -1,6 +1,16 @@
 package es.adriiiprieto.nasaapi.ui.fragment.list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ListViewModel : ViewModel() {
+
+    fun requestInformation() {
+        viewModelScope.launch(Dispatchers.IO) {
+            
+        }
+    }
+
 }
