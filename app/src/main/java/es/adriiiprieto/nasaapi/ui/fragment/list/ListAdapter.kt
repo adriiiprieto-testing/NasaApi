bb83511dce4011df1e49e888d9dd05425dfd4135
Adrián <adriiiprieto@gmail.com>
2021-02-08 22:39:19 +0100

    F001 RecyclerView

---
+++ app/src/main/java/es/adriiiprieto/nasaapi/ui/fragment/list/ListAdapter.kt
@@ -0,0 +1,41 @@
+package es.adriiiprieto.nasaapi.ui.fragment.list
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import es.adriiiprieto.nasaapi.R
+import es.adriiiprieto.nasaapi.data.model.Item
+import es.adriiiprieto.nasaapi.databinding.ItemNasaListBinding
+
+class ListAdapter(private var dataSet: List<Item>, private val context: Context) : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+
+    class ViewHolder(val binding: ItemNasaListBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemNasaListBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false))
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.binding.apply {
+            val item = dataSet[position]
+
+            itemNasaListTextViewTitle.text = item.data.firstOrNull()?.title ?: "N/A"
+            itemNasaListTextViewDescription.text = item.data.firstOrNull()?.description_508 ?: "N/A"
+
+            Glide.with(context)
+                .load(item.links.firstOrNull()?.href ?: "https://image.freepik.com/free-vector/page-found-error-404_23-2147508324.jpg")
+                .centerCrop()
+                .placeholder(R.drawable.ic_navegador)
+                .into(itemNasaListImageView)
+        }
+    }
+
+    override fun getItemCount() = dataSet.size
+
+    fun updateList(newList: List<Item>){
+        dataSet = newList
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/es/adriiiprieto/nasaapi/ui/fragment/list/ListFragment.kt
+++ app/src/main/java/es/adriiiprieto/nasaapi/ui/fragment/list/ListFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
 import es.adriiiprieto.nasaapi.R
 import es.adriiiprieto.nasaapi.databinding.FragmentListBinding
 
@@ -16,11 +19,21 @@ class ListFragment : Fragment() {
 
     val viewModel: ListViewModel by viewModels()
 
+    lateinit var adapter: ListAdapter
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View {
         binding = FragmentListBinding.inflate(inflater, container, false)
 
+        adapter = ListAdapter(listOf(), requireActivity())
+
+        binding.fragmentListRecyclerView.apply {
+            adapter = this@ListFragment.adapter
+            layoutManager = LinearLayoutManager(requireActivity(), LinearLayoutManager.VERTICAL, false)
+            itemAnimator = DefaultItemAnimator()
+        }
+
         viewModel.response.observe(viewLifecycleOwner, { response ->
-            Log.e("Ejemplo", response.toString())
+            adapter.updateList(response)
         })
 
         viewModel.requestInformation()
