94a5c859a0cd681e1eb89251a46d1c4d7f303e10
Adrián <adriiiprieto@gmail.com>
2021-02-10 22:03:13 +0100

    F003 Date formats and Kotlin extensions

--- app/src/main/java/es/adriiiprieto/nasaapi/ui/fragment/detail/DetailFragment.kt
+++ app/src/main/java/es/adriiiprieto/nasaapi/ui/fragment/detail/DetailFragment.kt
@@ -3,19 +3,17 @@ package es.adriiiprieto.nasaapi.ui.fragment.detail
 import android.app.SearchManager
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.SimpleAdapter
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import es.adriiiprieto.nasaapi.R
+import es.adriiiprieto.nasaapi.base.toMyDateFormat
 import es.adriiiprieto.nasaapi.data.model.Item
 import es.adriiiprieto.nasaapi.databinding.FragmentDetailBinding
 
@@ -27,7 +25,11 @@ class DetailFragment : Fragment() {
 
     val args: DetailFragmentArgs by navArgs()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding = FragmentDetailBinding.inflate(inflater, container, false)
 
         setupView(args.item)
@@ -38,10 +40,14 @@ class DetailFragment : Fragment() {
     private fun setupView(item: Item) {
 
         binding.fragmentDetailTextViewTitle.text = item.data.firstOrNull()?.title ?: ""
-        binding.fragmentDetailTextViewDate.text = item.data.firstOrNull()?.date_created ?: "Fecha desconocida"
+        binding.fragmentDetailTextViewDate.text =
+            item.data.firstOrNull()?.date_created?.toMyDateFormat() ?: "Fecha desconocida"
 
         Glide.with(requireActivity())
-            .load(item.links.firstOrNull()?.href ?: "https://image.freepik.com/free-vector/page-found-error-404_23-2147508324.jpg")
+            .load(
+                item.links.firstOrNull()?.href
+                    ?: "https://image.freepik.com/free-vector/page-found-error-404_23-2147508324.jpg"
+            )
             .centerCrop()
             .placeholder(R.drawable.ic_navegador)
             .into(binding.fragmentDetailImageViewNasa)
@@ -50,7 +56,7 @@ class DetailFragment : Fragment() {
             searchWeb(item.href)
         }
 
-        val description = when{
+        val description = when {
             !item.data.firstOrNull()?.description.isNullOrEmpty() -> item.data.firstOrNull()?.description
             !item.data.firstOrNull()?.description_508.isNullOrEmpty() -> item.data.firstOrNull()?.description_508
             else -> "No hay descripción"
