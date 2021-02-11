6c979447136dee031917359600b1128bba0e2deb
Adrián <adriiiprieto@gmail.com>
2021-02-11 20:18:01 +0100

    F004 State management in DetailFragment

--- app/src/main/java/es/adriiiprieto/nasaapi/ui/fragment/detail/DetailViewModel.kt
+++ app/src/main/java/es/adriiiprieto/nasaapi/ui/fragment/detail/DetailViewModel.kt
@@ -1,8 +1,19 @@
 package es.adriiiprieto.nasaapi.ui.fragment.detail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import es.adriiiprieto.nasaapi.base.BaseState
+import es.adriiiprieto.nasaapi.data.model.Item
 
 class DetailViewModel : ViewModel() {
+
+    private val state = MutableLiveData<BaseState>()
+    fun getState(): LiveData<BaseState> = state
+
+    fun setupParams(item: Item) {
+        state.postValue(BaseState.Normal(DetailState(item)))
+    }
 
 
 }
